256bbdb412439e9f9083ad7d74502412a0547cbd
ThiagoMaurat <thiagomaurat@hotmail.com>
2022-11-18 22:02:04 -0300

    feat:mainPage

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,14 +1,75 @@
+import appPreviewImage from "../assets/app-nlw-copa-preview.png";
+import logoImg from "../assets/logo.svg";
+import usersAvatarExampleImg from "../assets/avatares.png";
+import Image from "next/image";
+import iconCheckImage from "../assets/iconcheck.svg";
+
 interface HomeProps {
   count: number;
 }
 
 export default function Home(props: HomeProps) {
   return (
-    <h1 className="text-violet-500 font-bold">{`Contagem: ${props.count}`}</h1>
+    <div className="max-w-[1124px] h-screen mx-auto grid grid-cols-2 items-center gap-28">
+      <main>
+        <Image src={logoImg} alt="logoImg" />
+
+        <h1 className="mt-14 text-white text-5xl font-bold leading-tight">
+          Crie seu próprio bolão da copa e compartilhe entre amigos!
+        </h1>
+
+        <div className="mt-10 flex items-center gap-2">
+          <Image src={usersAvatarExampleImg} alt="" />
+          <strong className="text-gray-100 text-xl">
+            <span className="text-ignite-500">+12.592</span> pessoas já estão
+            usando
+          </strong>
+        </div>
+
+        <form className="mt-10 flex gap-2">
+          <input
+            className="flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm"
+            type={"text"}
+            required
+            placeholder="Qual o nome do seu bolão "
+          />
+          <button
+            className="bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700"
+            type="submit"
+          >
+            Criar um bolão
+          </button>
+        </form>
+
+        <p className="mt-4 text-sm text-gray-300 leading-relaxed">
+          Após criar seu bolão, você receberá um código único que poderá usar
+          para convidar seus outras pessoas
+        </p>
+
+        <div className="mt-10 pt-10 border-t border-gray-600">
+          <div>
+            <Image src={iconCheckImage} alt="check" />
+            <div>
+              <span>+2.034</span>
+              <span>Bolões criados</span>
+            </div>
+          </div>
+          <div>
+            <Image src={iconCheckImage} alt="check" />
+            <div>
+              <span>+2.034</span>
+              <span>Bolões criados</span>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <Image src={appPreviewImage} alt="nlwImage" />
+    </div>
   );
 }
 
-export const getServerSideProps = async () => {
+/* export const getServerSideProps = async () => {
   const response = await fetch("http://localhost:3333/pools/count");
   const data = await response.json();
 
@@ -17,4 +78,4 @@ export const getServerSideProps = async () => {
       count: data.count,
     },
   };
-};
+}; */
